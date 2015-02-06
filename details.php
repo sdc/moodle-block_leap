36cdb27169b6e97af6e80606a339b74137bf34d2
Vaughany <paulieboo@gmail.com>
2015-02-06 17:02:35 +0000

    Moved lang strings and sorted out links to it in details.php.

--- details.php
+++ details.php
@@ -1,50 +1,50 @@
 <?php
 
 $trackertypes = array(
-    'core'      => get_string( 'tracker_type:core', 'block_leapgradetracking' ),
-    'english'   => get_string( 'tracker_type:english', 'block_leapgradetracking' ),
-    'maths'     => get_string( 'tracker_type:maths', 'block_leapgradetracking' ),
-    'ppd'       => get_string( 'tracker_type:ppd', 'block_leapgradetracking' ),
-    'test'      => get_string( 'tracker_type:test', 'block_leapgradetracking' ),
-);       
+    'core'                  => get_string( 'tracker_type:core', 'block_leapgradetracking' ),
+    'english'               => get_string( 'tracker_type:english', 'block_leapgradetracking' ),
+    'maths'                 => get_string( 'tracker_type:maths', 'block_leapgradetracking' ),
+    'ppd'                   => get_string( 'tracker_type:ppd', 'block_leapgradetracking' ),
+    'test'                  => get_string( 'tracker_type:test', 'block_leapgradetracking' ),
+);
 
 $coursetypes = array(
-    'null' => array (
-        ''                  => 'None',
+    'none' => array (
+        ''                  => get_string( 'course_type:none', 'block_leapgradetracking' ),,
     ),
 
     'a2' => array (
-        'a2_artdes'         => 'Art &amp; Design',
-        'a2_artdesphoto'    => 'Art &amp; Design (Photo)',
-        'a2_artdestext'     => 'Art &amp; Design (Textiles)',
-        'a2_biology'        => 'Biology',
-        'a2_busstud'        => 'Business Studies',
-        'a2_chemistry'      => 'Chemistry',
-        'a2_englishlang'    => 'English Language',
-        'a2_englishlit'     => 'English Literature',
-        'a2_envsci'         => 'Environmental Science',
-        'a2_envstud'        => 'Environmental Studies',
-        'a2_filmstud'       => 'Film Studies',
-        'a2_geography'      => 'Geography',
-        'a2_govpoli'        => 'Government &amp; Politics',
-        'a2_history'        => 'History',
-        'a2_humanbiology'   => 'Human Biology',
-        'a2_law'            => 'Law',
-        'a2_maths'          => 'Maths',
-        'a2_mathsfurther'   => 'Further Maths',
-        'a2_media'          => 'Media',
-        'a2_philosophy'     => 'Philosophy',
-        'a2_physics'        => 'Physics',
-        'a2_psychology'     => 'Psychology',
-        'a2_sociology'      => 'Sociology',
+        'a2_artdes'         => get_string( 'course_type:a2_artdes', 'block_leapgradetracking' ),
+        'a2_artdesphoto'    => get_string( 'course_type:a2_artdesphoto', 'block_leapgradetracking' ),
+        'a2_artdestext'     => get_string( 'course_type:a2_artdestext', 'block_leapgradetracking' ),
+        'a2_biology'        => get_string( 'course_type:a2_biology', 'block_leapgradetracking' ),
+        'a2_busstud'        => get_string( 'course_type:a2_busstud', 'block_leapgradetracking' ),
+        'a2_chemistry'      => get_string( 'course_type:a2_chemistry', 'block_leapgradetracking' ),
+        'a2_englishlang'    => get_string( 'course_type:a2_englishlang', 'block_leapgradetracking' ),
+        'a2_englishlit'     => get_string( 'course_type:a2_englishlit', 'block_leapgradetracking' ),
+        'a2_envsci'         => get_string( 'course_type:a2_envsci', 'block_leapgradetracking' ),
+        'a2_envstud'        => get_string( 'course_type:a2_envstud', 'block_leapgradetracking' ),
+        'a2_filmstud'       => get_string( 'course_type:a2_filmstud', 'block_leapgradetracking' ),
+        'a2_geography'      => get_string( 'course_type:a2_geography', 'block_leapgradetracking' ),
+        'a2_govpoli'        => get_string( 'course_type:a2_govpoli', 'block_leapgradetracking' ),
+        'a2_history'        => get_string( 'course_type:a2_history', 'block_leapgradetracking' ),
+        'a2_humanbiology'   => get_string( 'course_type:a2_humanbiology', 'block_leapgradetracking' ),
+        'a2_law'            => get_string( 'course_type:a2_law', 'block_leapgradetracking' ),
+        'a2_maths'          => get_string( 'course_type:a2_maths', 'block_leapgradetracking' ),
+        'a2_mathsfurther'   => get_string( 'course_type:a2_mathsfurther', 'block_leapgradetracking' ),
+        'a2_media'          => get_string( 'course_type:a2_media', 'block_leapgradetracking' ),
+        'a2_philosophy'     => get_string( 'course_type:a2_philosophy', 'block_leapgradetracking' ),
+        'a2_physics'        => get_string( 'course_type:a2_physics', 'block_leapgradetracking' ),
+        'a2_psychology'     => get_string( 'course_type:a2_psychology', 'block_leapgradetracking' ),
+        'a2_sociology'      => get_string( 'course_type:a2_sociology', 'block_leapgradetracking' ),
     ),
 
     'btec' => array(
-        'btec_ed_applsci'   => 'Extended Diploma in Applied Science',
+        'btec_ed_applsci'   => get_string( 'course_type:btec_ed_applsci', 'block_leapgradetracking' ),
     ),
 
     'gcse' => array (
-        'gcse_english'      => 'English',
-        'gcse_maths'        => 'Maths',
+        'gcse_english'      => get_string( 'course_type:gcse_english', 'block_leapgradetracking' ),
+        'gcse_maths'        => get_string( 'course_type:gcse_maths', 'block_leapgradetracking' ),
     ),
 );
--- lang/en/block_leapgradetracking.php
+++ lang/en/block_leapgradetracking.php
@@ -3,22 +3,22 @@
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
+// the Free Software Foundation; either version 3 of the License; or
 // (at your option) any later version.
 //
-// Moodle is distributed in the hope that it will be useful,
+// Moodle is distributed in the hope that it will be useful;
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// along with Moodle.  If not; see <http://www.gnu.org/licenses/>.
 
 /**
- * Strings for component 'block_leapgradetracking', language 'en'.
+ * Strings for component 'block_leapgradetracking'; language 'en'.
  *
  * @package    block_leapgradetracking
- * @copyright  2014, 2015 Paul Vaughan {@link http://commoodle.southdevon.ac.uk}
+ * @copyright  2014; 2015 Paul Vaughan {@link http://commoodle.southdevon.ac.uk}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -38,3 +38,38 @@ $string['tracker_type:english'] = 'English';
 $string['tracker_type:maths']   = 'Maths';
 $string['tracker_type:ppd']     = 'PPD';
 $string['tracker_type:test']    = 'Test (for development and testing purposes only)';
+
+$string['course_type']          = 'Course type';
+
+$string['course_type_title:none']   = 'None:';
+$string['course_type_title:a2']     = 'AS/A2:';
+$string['course_type_title:btec']   = 'BTEC:';
+$string['course_type_title:gcse']   = 'GCSE:';
+
+$string['course_type:none']             = 'None';
+$string['course_type:a2_artdes']        = 'Art &amp; Design';
+$string['course_type:a2_artdesphoto']   = 'Art &amp; Design (Photography)';
+$string['course_type:a2_artdestext']    = 'Art &amp; Design (Textiles)';
+$string['course_type:a2_biology']       = 'Biology';
+$string['course_type:a2_busstud']       = 'Business Studies';
+$string['course_type:a2_chemistry']     = 'Chemistry';
+$string['course_type:a2_englishlang']   = 'English Language';
+$string['course_type:a2_englishlit']    = 'English Literature';
+$string['course_type:a2_envsci']        = 'Environmental Science';
+$string['course_type:a2_envstud']       = 'Environmental Studies';
+$string['course_type:a2_filmstud']      = 'Film Studies';
+$string['course_type:a2_geography']     = 'Geography';
+$string['course_type:a2_govpoli']       = 'Government &amp; Politics';
+$string['course_type:a2_history']       = 'History';
+$string['course_type:a2_humanbiology']  = 'Human Biology';
+$string['course_type:a2_law']           = 'Law';
+$string['course_type:a2_maths']         = 'Maths';
+$string['course_type:a2_mathsfurther']  = 'Further Maths';
+$string['course_type:a2_media']         = 'Media';
+$string['course_type:a2_philosophy']    = 'Philosophy';
+$string['course_type:a2_physics']       = 'Physics';
+$string['course_type:a2_psychology']    = 'Psychology';
+$string['course_type:a2_sociology']     = 'Sociology';
+$string['course_type:btec_ed_applsci']  = 'Extended Diploma in Applied Science';
+$string['course_type:gcse_english']     = 'English';
+$string['course_type:gcse_maths']       = 'Maths';
