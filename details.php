177bd488d1c4c5fb2f8f3ff6aac7d5d0ab466741
Paul Vaughan <paulieboo@gmail.com>
2015-02-10 14:42:26 +0000

    Pertinent details moved from the overnight script into details.php: will edit some other time.

--- details.php
+++ details.php
@@ -6,6 +6,9 @@
  * TODO: Potentially sort the nested arrays (e.g. in alphabetical order) instead of just adding them in in the required order.
  */
 
+/**
+ * Differrent types of tracker.
+ */
 $trackertypes = array (
     get_string( 'tracker_type_title:none', 'block_leapgradetracking' ) => array (
         ''          => get_string( 'tracker_type:none', 'block_leapgradetracking' ),
@@ -23,6 +26,9 @@ $trackertypes = array (
     ),
 );
 
+/**
+ * Differrent types of course for tracking.
+ */
 $coursetypes = array (
     get_string( 'course_type_title:none', 'block_leapgradetracking' ) => array (
         ''          => get_string( 'course_type:none', 'block_leapgradetracking' ),
@@ -62,4 +68,40 @@ $coursetypes = array (
         'gcse_english'      => get_string( 'course_type:gcse_english', 'block_leapgradetracking' ),
         'gcse_maths'        => get_string( 'course_type:gcse_maths', 'block_leapgradetracking' ),
     ),
+);
+
+/**
+ * Numbers required for making a MAG from a L3VA.
+ */
+$l3va_data = array(
+    'a2_artdes'         => array( 'm' => 4.4727, 'c' => 98.056 ),
+    'a2_artdesphoto'    => array( 'm' => 4.1855, 'c' => 79.949 ),
+    'a2_artdestext'     => array( 'm' => 3.9430, 'c' => 66.967 ),
+    'a2_biology'        => array( 'm' => 5.2471, 'c' => 166.67 ),
+    'a2_busstud'        => array( 'm' => 4.8372, 'c' => 123.41 ),
+    'a2_chemistry'      => array( 'm' => 4.5169, 'c' => 129.00 ),
+    'a2_englishlang'    => array( 'm' => 4.5773, 'c' => 112.14 ),
+    'a2_englishlit'     => array( 'm' => 5.0872, 'c' => 137.31 ),
+    'a2_envsci'         => array( 'm' => 6.1058, 'c' => 196.66 ),
+    'a2_envstud'        => array( 'm' => 6.1058, 'c' => 196.66 ),
+    'a2_filmstud'       => array( 'm' => 4.0471, 'c' => 76.470 ),
+    'a2_geography'      => array( 'm' => 5.4727, 'c' => 156.16 ),
+    'a2_govpoli'        => array( 'm' => 5.3215, 'c' => 145.38 ),
+    'a2_history'        => array( 'm' => 4.6593, 'c' => 118.98 ),
+    'a2_humanbiology'   => array( 'm' => 5.2471, 'c' => 166.67 ),
+    'a2_law'            => array( 'm' => 5.1047, 'c' => 140.69 ),
+    'a2_maths'          => array( 'm' => 4.5738, 'c' => 119.43 ),
+    'a2_mathsfurther'   => array( 'm' => 4.4709, 'c' => 106.40 ),
+    'a2_media'          => array( 'm' => 4.2884, 'c' => 90.279 ),
+    'a2_philosophy'     => array( 'm' => 4.7645, 'c' => 128.95 ),
+    'a2_physics'        => array( 'm' => 5.0965, 'c' => 159.08 ),
+    'a2_psychology'     => array( 'm' => 5.3872, 'c' => 158.71 ),
+    'a2_sociology'      => array( 'm' => 4.9645, 'c' => 122.95 ),
+
+    'btecex_applsci'    => array( 'm' => 10.606, 'c' => 269.15 ),
+
+    'default'           => array( 'm' => 4.8008, 'c' => 126.18 ),
+
+    'btec'              => array( 'm' => 3.9, 'c' => 90 ),
+
 );
