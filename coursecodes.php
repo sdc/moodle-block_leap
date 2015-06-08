5d515e5038dc02203b822023e4cfa27543a09310
Paul Vaughan <paulieboo@gmail.com>
2015-06-08 13:59:17 +0100

    json course codes improvements.

--- coursecodes.php
+++ coursecodes.php
@@ -4713,4 +4713,12 @@ foreach ( $codes as $key => $value ) {
     }
 }
 
+// JSON content type.
+//header( 'Content-Type: application/json' );
+header( 'Content-Type: text/plain' );
+
+// Don't cache this!
+//header( 'Cache-Control: no-cache, must-revalidate' ); // HTTP/1.1
+//header( 'Expires: Sat, 26 Jul 1997 05:00:00 GMT' ); // Date in the past
+
 echo json_encode( $out );
